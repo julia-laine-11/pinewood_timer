7292d97c3c839faa665eb28509fe7629306a1c98
Julia Laine <julia@thelaines.net>
2025-07-12 18:06:31 -0400

    made some test code for placement display

--- Software/code/pinewood_program/src/main.c
+++ Software/code/pinewood_program/src/main.c
@@ -2,65 +2,45 @@
 #include <stdint.h>
 #include <stdio.h>
 
+int disp[4] = {1, 2, 3, 4};
 
-// void init_gpioa(){
-//     RCC -> AHBENR |= RCC_AHBENR_GPIOAEN;
-//     GPIOA -> MODER &= ~0xFFC00000;
-//     GPIOA -> MODER &= ~0x3FFFF;
-//     GPIOA -> MODER |=  0x15555;
-//     GPIOA->PUPDR &= ~(0xFF);         // Clear pull-up/down bits for PA0–PA3
-//     GPIOA->PUPDR |=  (0x55);         // Set PA0–PA3 to pull-up (01) or pull-down (10)
-// }
 
-// int read_pin(){
-//     int pin_val = GPIOA -> IDR;
-//     pin_val &= 0xF;
-//     return pin_val;
-// }
+//1
+//110
 
-// void TIM7_IRQHandler(){
-//     //acknowledge
-//     TIM7 -> SR &= ~TIM_SR_UIF;  
-//     //if row != 0, convert the key to usable value (thrust, mode, etc)
-//     int row = read_pin();
-//         // GPIOA -> BSRR = 1 << 2;
-//         // GPIOA -> BRR  = 1 << 3;
-    
-  
-//   }
-  
-//   /**
-//    * @brief Setup timer 7 as described in lab handout
-//    * 
-//    */
-//   void setup_tim7() {
-//     //Enable RCC clock for TIM7
-//     RCC -> APB1ENR |= RCC_APB1ENR_TIM7EN;
+//2
+//1011011
 
-//     TIM7 -> PSC = 480 - 1; //48 000 000 / 480 -> 100 000
-//     TIM7 -> ARR = 100 - 1; //100 000 / 100 -> 1kHz
-  
-//     //Enable UIE bit in DIER (use TIM_DIER_UIE mask)
-//     TIM7 -> DIER |= TIM_DIER_UIE;
-//     //Enable the TIM7 interrupt (NVIC ISER)
-//     NVIC -> ISER[0] |= (1 << TIM7_IRQn);
-//     //Enable TIM7 by setting CEN bit in TIM7 CR)  - set TIM_CR1_CEN in TIM7_CR1 (dont set the TIM7_ARR to 0)
-//     TIM7 -> CR1 |= TIM_CR1_CEN;
-//   }
-  
+//3
+//0b1001111
+
+//4
+//0b1100110
+
+void enable_ports() {
+    RCC -> AHBENR |= RCC_AHBENR_GPIOAEN;
+
+    //segments
+    GPIOA -> MODER &=  ~0x3FFF;
+    GPIOA -> MODER |=   0x1555;
+
+    //enable
+    // 0000 0000 0000 0000 0000 0000 0000 0000
+    // 1514 1312 1110 0908 0706 0504 0302 0100
+    // ---- ---- 0101 0101 ---- ---- ---- ----
+    GPIOA -> MODER &= ~0xFF0000;
+    GPIOA -> MODER |=  0x550000;
+}
 
 int main(){
 
-    // internal_clock();
-    // init_gpioa();
-    // // GPIOA -> BSRR = 1 << 2;
-
-    // setup_tim7();
-
-
-    // while (1) {
-    //     __WFI();  // Wait for interrupt (optional, saves power)
-    // }
+    internal_clock();
+    enable_ports();
+    // 0000 0000 0000 0000
+    //       098 7654 3210
+    // ---- -111 1
+    GPIOA -> BSRR = 0b1000000000;
+    GPIOA -> BSRR = 0b110;
     
     return 1;
 }
